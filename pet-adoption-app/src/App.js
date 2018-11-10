26aa0b1ccf0a838bcc86c999e842f3ab76dffc00
SL0TR <mohaiminislam94@gmail.com>
2018-11-10 15:10:51 +0600

    add eslint, prettier, parcel and configure them

--- pet-adoption-app/src/App.js
+++ pet-adoption-app/src/App.js
@@ -1,35 +1,18 @@
-const Pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h2', {}, props.animal),
-    React.createElement('h3', {}, props.breed),
-  ])
-}
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert("You clicked the title")
-  }
   render() {
-    return React.createElement('div', {}, [
-      React.createElement('h1', { onClick: this.handleTitleClick }, 'Adopt Me!'),
-      React.createElement(Pet, {
-        name: 'Luna',
-        animal: 'Dog',
-        breed: 'Havanese'
-      }),
-      React.createElement(Pet, {
-        name: 'Pepper',
-        animal: 'bird',
-        breed: 'Cockatiel'
-      }),
-      React.createElement(Pet, {
-        name: 'Doink',
-        animal: 'cat',
-        breed: 'Mixed'
-      })
-    ])
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        <Pet name="Luna" animal="dog" breed="Havanese" />
+        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+        <Pet name="Doink" animal="cat" breed="Mixed" />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+render(<App />, document.getElementById("root"));
